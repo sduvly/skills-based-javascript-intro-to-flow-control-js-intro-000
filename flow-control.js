be909db7878be270d57ede39a8f26f1cae6f356c
beautiful-inheritance-1081 <sduvly@gmail.com>
2021-03-31 03:29:05 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,41 +1,42 @@
 function basicTeenager(age) {
-if (13) {
-return teenager
+if (13 <= 19) {
+return "You are a teenager"
 } else {
-  return not a teenager
-  {
+  return "You are not a teenager"
+}
 }
 
 function teenager(age) {
   if (13 >= 19) {
-  return you are a teenager
+  return "You are a teenager"
 
 } else {
-  return you are not a teenager
+  return "You are not a teenager"
+   }
 }
 
 function ageChecker(age) {
 if (0 >= 13 || 19 < 100) {
-return You are a kid
+return "You are a kid"
 } else {
-  return You are a grownup
+  return "You are a grownup"
 } else if (13 <= 19) {
-  return you are a teenager
-}
+  return "you are a teenager"
+  }
 }
 
 function ternaryTeenager(age) {
-  if (13) {
-  return teenager
+  if (13 <= 19) {
+  return You are a teenager
   } else {
-    return not a teenager
-    {
+    return "You have an age"
+  }
 }
 
 function switchAge(age) {
-  if (13) {
+  if (13 <= 19) {
   return teenager
   } else {
     return not a teenager
-    {
+  }
 }
